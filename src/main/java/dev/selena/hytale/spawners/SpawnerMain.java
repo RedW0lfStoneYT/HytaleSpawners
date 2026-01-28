a4f148d6b2d4c9a407728b2119051d5d4ce40ce1
Selena Rose Hammond <selenarosehammond@gmail.com>
2026-01-29 07:21:33 +1300

    Added component and system registration for previously mentioned stuff

--- src/main/java/dev/selena/hytale/spawners/SpawnerMain.java
+++ src/main/java/dev/selena/hytale/spawners/SpawnerMain.java
@@ -4,10 +4,14 @@ import com.hypixel.hytale.component.ComponentType;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import dev.selena.core.HytaleCore;
 import dev.selena.hytale.spawners.blockstates.SpawnerBlock;
 import dev.selena.hytale.spawners.commands.SpawnerGiveCommand;
+import dev.selena.hytale.spawners.components.DisplayEntityComponent;
+import dev.selena.hytale.spawners.components.NerfedMobComponent;
 import dev.selena.hytale.spawners.systems.SpawnerBlockSystem;
+import dev.selena.hytale.spawners.util.config.Config;
 import dev.selena.hytale.spawners.util.config.Configs;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
@@ -16,6 +20,8 @@ public class SpawnerMain extends JavaPlugin {
 
     @Getter
     private ComponentType<ChunkStore, SpawnerBlock> spawnerBlockComponentType;
+    @Getter
+    private ComponentType<EntityStore, DisplayEntityComponent> spawnerEntityComponentType;
     private static SpawnerMain instance;
 
     public SpawnerMain(@NotNull JavaPluginInit init) {
@@ -29,9 +35,14 @@ public class SpawnerMain extends JavaPlugin {
     protected void setup() {
 
         this.spawnerBlockComponentType = getChunkStoreRegistry().registerComponent(SpawnerBlock.class, "Spawner", SpawnerBlock.CODEC);
+        this.spawnerEntityComponentType = getEntityStoreRegistry().registerComponent(DisplayEntityComponent.class, "DisplayEntity", DisplayEntityComponent.CODEC);
+        NerfedMobComponent.setComponentType(getEntityStoreRegistry().registerComponent(NerfedMobComponent.class, "NerfedMob", NerfedMobComponent.CODEC));
         getEntityStoreRegistry().registerSystem(new SpawnerBlockSystem.SpawnerPlaceSystem());
+        if (Config.get().isRotatePreviewEntity())
+            getEntityStoreRegistry().registerSystem(new SpawnerBlockSystem.PreviewRotating());
         getChunkStoreRegistry().registerSystem(new SpawnerBlockSystem.Ticking());
         getChunkStoreRegistry().registerSystem(new SpawnerBlockSystem.OnSpawnerBlockAdd());
+        getEntityStoreRegistry().registerSystem(new SpawnerBlockSystem.NerfedMobDeath());
         getCommandRegistry().registerCommand(new SpawnerGiveCommand());
 
     }
